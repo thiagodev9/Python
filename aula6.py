8145aad3aa56f77a480c4148a3af99a511fc3ffd
Thiago <thiagodessa41@gmail.com>
2026-03-31 16:48:14 -0300

    Ferramentas usadas boas praticas codigo

---
+++ aula6.py
@@ -0,0 +1,32 @@
+"""
+Sempre que for definir padrões de projeto, utilize o padrão pre-commit.
+-black
+-flake8
+-isort
+-pre-commit
+-bandit
+-commitezer
+"""
+
+# Exemplo de pre-commit em Python
+
+# pre-commit é uma ferramenta que automatiza a execução de verificações de qualidade de código antes de cada commit.
+# Ele pode ser usado para verificar a formatação do código, a presença de erros de sintaxe, vulnerabilidades de segurança, etc.
+
+# Exemplo de uso
+# pre-commit install
+# pre-commit run
+# pre-commit run --all-files
+# black é uma ferramenta que formata o código automaticamente
+# flake8 é uma ferramenta que verifica erros de sintaxe
+# isort é uma ferramenta que ordena as importações
+# bandit é uma ferramenta que verifica vulnerabilidades de segurança
+# commitezer é uma ferramenta que gera mensagens de commit
+    
+
+
+
+
+
+
+
